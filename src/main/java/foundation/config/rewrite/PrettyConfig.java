cfd9168fe2bdeb026de1e0505530045d9240c322
seth.ellison <seth.ellison@mis11.FDLIC.private.dig-inc.net>
2014-12-16 10:00:15 -0600

    PrettyFaces is now loading, but @URLMapping is still not functioning. I added an application.properties entry in attempt to configure the embedded Tomcat server, but it doesn't seem to be working.
    Perhaps I'm missing something else.

--- src/main/java/foundation/config/rewrite/PrettyConfig.java
+++ src/main/java/foundation/config/rewrite/PrettyConfig.java
@@ -7,18 +7,21 @@ import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-public class PrettyConfig
-{
+public class PrettyConfig {
 	@Bean
 	public FilterRegistrationBean prettyFilter() {
-	    FilterRegistrationBean prettyFilter = new FilterRegistrationBean(new RewriteFilter());
-	    prettyFilter.setDispatcherTypes(DispatcherType.FORWARD, DispatcherType.REQUEST,
-	            DispatcherType.ASYNC, DispatcherType.ERROR);
-	    prettyFilter.addUrlPatterns("/*");
-	    
-	    prettyFilter.addInitParameter("com.ocpsoft.pretty.BASE_PACKAGES", "foundation");
-	    return prettyFilter;
+		FilterRegistrationBean prettyFilter = new FilterRegistrationBean(new RewriteFilter());
+		
+		prettyFilter.setDispatcherTypes(DispatcherType.FORWARD,
+										DispatcherType.REQUEST,
+										DispatcherType.ASYNC,
+										DispatcherType.ERROR);
+		
+		prettyFilter.addUrlPatterns("/*");
+
+		//prettyFilter.addInitParameter("com.ocpsoft.pretty.BASE_PACKAGES", "foundation");
+		
+		return prettyFilter;
 	}
 }
--- src/main/java/foundation/config/rewrite/RewriteConfig.java
+++ src/main/java/foundation/config/rewrite/RewriteConfig.java
@@ -1,36 +1,37 @@
 package foundation.config.rewrite;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.ServletContext;
-/*
+
 import org.ocpsoft.logging.Logger.Level;
 import org.ocpsoft.rewrite.annotation.RewriteConfiguration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
 import org.ocpsoft.rewrite.config.Log;
+import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-*/
 
 //@Configuration
 //@RewriteConfiguration
 public class RewriteConfig //extends HttpConfigurationProvider
 {
 	/*
-	 ### Commented out because "Rewrite is active" never prints out, and the configuration seems to never work. ###
 	@Override
 	public int priority()
 	{
 		return 10;
 	}
 	
-   @Override
-   public org.ocpsoft.rewrite.config.Configuration getConfiguration(ServletContext context)
-   {
-	  System.err.println("Rewrite getConfiguration() executed.");
-      return ConfigurationBuilder.begin()
-         .addRule()
-         .perform(Log.message(Level.INFO, "Rewrite is active."))
-      ;
-   }*/
-
+	@Override
+	public org.ocpsoft.rewrite.config.Configuration getConfiguration(ServletContext context)
+	{
+		System.err.println("Rewrite getConfiguration() executed.");
+		return ConfigurationBuilder.begin()
+				.addRule()
+				.perform(Log.message(Level.INFO, "Rewrite is active.")) ;
+   }
+   */
    
 }
